3b785b59ff4c34020927830418256c575e3658ef
FelipeMCassiano <felipemcassiano@gmail.com>
2024-04-27 12:51:45 -0300

    feat: change colorscheme

--- lua/martian/colors.lua
+++ lua/martian/colors.lua
@@ -39,6 +39,6 @@ colors.comment = "#555568"
 colors.symbol = "#bbbbbb"
 colors.primary = "#ffffff"
 colors.terminalBrightBlack = "#1f1f29"
-colors.redlight = "#816271"
+colors.redlight = "#c3a38a"
 
 return colors
